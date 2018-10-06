0d3a1e334ba569e6d8dc28ab03b477f53833e48f
Ezra T Huffman <joshooha@gmail.com>
2018-10-06 20:40:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,12 +11,16 @@ class TicTacToe
   [ 2, 5, 8 ]
   ]
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def initialize
+  
+end
+
+def display_board
+  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
 def input_to_index(input)
